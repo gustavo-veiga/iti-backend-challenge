fafc08ae4a3fc56b7e7c293aa114c44ba928c76f
Gustavo Veiga <gustavo.veiga@iteris.com.br>
2021-11-03 14:44:10 -0300

    Update test controller

--- src/test/java/itau/iti/challenge/controller/PasswordControllerTest.java
+++ src/test/java/itau/iti/challenge/controller/PasswordControllerTest.java
@@ -8,16 +8,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import itau.iti.challenge.dto.DPasswordValidateRequest;
-import itau.iti.challenge.service.PasswordService;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 class PasswordControllerTest {
